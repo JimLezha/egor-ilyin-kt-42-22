3e9261ca4f562e2b66e91e0c0747eafa58ff6664
Egor Ilyin <dandyjimmy192@gmail.com>
2025-02-10 14:38:49 +0300

    PervoeNaVtoroe

--- egorIlyinKT-42-22/Program.cs
+++ egorIlyinKT-42-22/Program.cs
@@ -1,21 +1,41 @@
+using NLog;
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-var app = builder.Build();
+try
+{
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapRazorPages();
+
+    app.Run();
 }
-app.UseStaticFiles();
+catch(Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+}
+finally
+{
+    LogManager.Shutdown();
+}
 
-app.UseRouting();
+// Add services to the container.
 
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
